938ebc410d98c677beb85a123a670c9debe32711
Alex Defrese <aidandefrese@gmail.com>
2022-12-27 21:54:20 -0500

    set up graphQL server/backend functionality

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema');
 
 
 const app = express ();
 
 app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
-}));
+    schema: schema,
+    graphiql: true,
+  }));
 
 const PORT = process.env.PORT || 5000;
 
